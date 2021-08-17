4e4a2614917f8e29a63bc2874518ba4a0922f0d1
Codie Newark <git@codie.gg>
2021-08-17 21:05:35 +0100

    Added coin flip module.

---
+++ src/modules/CoinModule.ts
@@ -0,0 +1,20 @@
+import events from "../events"
+import IModule from "./IModule"
+import { Message } from "discord.js"
+
+export default class CoinModule implements IModule {
+    getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+    registerModule() {
+        events.onDiscordCommand((message : Message, name : string, args : string[]) => {
+            if (name == "flip") {
+                const lucky = this.getRandomIntInclusive(0, 1) == 1;
+                message.channel.send(`You flipped a coin, it landed on the **${lucky ? "Lucky" : "Unlucky"}** side.`)
+            }
+        })
+    }
+
+    getHelpText() {
+        return "`&flip\nFlip a coin.'"
+    }
+}
