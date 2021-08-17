add0200511f034b3f09eae3e9e77ef9ff1fcb76f
Codie Stella <git@codie.gg>
2021-08-17 19:56:17 +0100

    1.2.2 (#42)
    * Added Appveyor yml
    
    * Corrected typo
    
    * Updated appveyor version number.
    
    * Corrected build script.
    
    * Fixed build number.
    
    * Turned off the test script
    
    * Added artefact
    
    * Adjusted artifact
    
    * Adjusted Artifact
    
    * Load modules from .env file.
    
    * 1.2.0
    
    * Added latest release link to GitHub command.
    
    * 1.2.1
    
    * Moved twitterBotClient
    
    * 1.2.2

--- src/twitter/twitterBotClient.ts
+++
@@ -1,8 +0,0 @@
-import Twit from "twit"
-
-export default new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-})
