91fcea79590b12c7a2bc8d0b90cae65a6b2ca28f
Codie Stella <git@codie.gg>
2021-08-20 15:13:03 +0100

    �

--- build_scripts/redeploy.sh
+++ build_scripts/redeploy.sh
@@ -4,10 +4,7 @@ SCREEN_NAME=$(basename "$PWD")
 screen -S $SCREEN_NAME -X quit
 rm -rf dist
 git pull
-if [ ! -f .env ]
-then
-  export $(cat .env | xargs)
-fi
+set -o allexport; source .env; set +o allexport
 ./build_scripts/download-latest-build.sh --token $APPVEYOR_TOKEN
 unzip dist.zip
 ./build_scripts/start.sh
