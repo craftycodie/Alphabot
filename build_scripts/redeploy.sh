b64662dd9389909767ba232a9c049ca18ede2e63
Codie Stella <git@codie.gg>
2021-08-20 16:18:45 +0100

    Corrected a command.

--- build_scripts/redeploy.sh
+++ build_scripts/redeploy.sh
@@ -8,5 +8,5 @@ APPVEYOR_TOKEN=$(grep APPVEYOR_TOKEN .env | xargs)
 APPVEYOR_TOKEN=${APPVEYOR_TOKEN#*=}
 bash ./build_scripts/download-latest-build.sh --token $APPVEYOR_TOKEN
 unzip dist.zip -d dist
-rn -rf dist.zip
+rm -rf dist.zip
 bash ./build_scripts/start.sh
