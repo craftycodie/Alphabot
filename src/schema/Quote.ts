7479988e64b8aefef9f257d290d03c0748296a2c
Codie Stella <git@codie.gg>
2021-08-20 22:09:17 +0100

    Added quotes module.

---
+++ src/schema/Quote.ts
@@ -0,0 +1,26 @@
+import { Document, model, Schema } from "mongoose"
+import Quote from "../models/Quote"
+
+const QuoteSchema = new Schema<QuoteDocument>({
+  text: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  addedBy: {
+    type: String,
+    required: true,
+  },
+  quoteFrom: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+})
+
+interface QuoteDocument extends Document, Quote {}
+
+export default model<QuoteDocument>("Quote", QuoteSchema)
