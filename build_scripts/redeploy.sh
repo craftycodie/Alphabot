54fd4d32b026b1e05ec228effda97701fa72463a
codie <git@alex231.com>
2022-02-16 20:08:26 +0000

    Updated redeploy script.

--- build_scripts/redeploy.sh
+++ build_scripts/redeploy.sh
@@ -4,11 +4,7 @@ cd ..
 sleep 5s
 SCREEN_NAME=$(basename "$PWD")
 screen -S $SCREEN_NAME -X quit
-rm -rf dist/*
 git pull
-APPVEYOR_TOKEN=$(grep APPVEYOR_TOKEN .env | xargs)
-APPVEYOR_TOKEN=${APPVEYOR_TOKEN#*=}
-bash ./build_scripts/download-latest-build.sh --token $APPVEYOR_TOKEN
-unzip -q dist.zip -d dist
-rm -rf dist.zip
+npm i
+npm run build
 bash ./build_scripts/start.sh
